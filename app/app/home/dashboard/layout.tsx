63b3c6ddbbd8ffd3fb7ec6679b29f23c2b5d7287
Emmanuele <emmanuele.piola@gmail.com>
2025-05-19 03:01:56 +0200

    frontend update

--- app/app/home/dashboard/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return(
-    <div className="w-full h-full bg-white">
-        {children}
-    </div>
-  );
-}
