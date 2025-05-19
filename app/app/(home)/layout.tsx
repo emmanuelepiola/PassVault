43ac93bd2edb423149cc1957192e52b812022421
Emmanuele <emmanuele.piola@gmail.com>
2025-05-19 03:06:10 +0200

    frontend error fix

--- app/app/(home)/layout.tsx
+++ app/app/(home)/layout.tsx
@@ -10,19 +10,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   
 
   return(
-    
-    <html lang="en">
-      <head>
-      <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
-      </head>
-      <body className="h-[100vh] w-[100vw] overflow-hidden flex flex-row bg-white">
+      <div className="h-[100vh] w-[100vw] overflow-hidden flex flex-row bg-white">
         <SelectionProvider>
           <Sidebar/>
           <div className="h-full w-full pl-[20rem]" >
             {children}
           </div>
         </SelectionProvider>
-      </body>
-    </html>
+      </div>
   );
 }
