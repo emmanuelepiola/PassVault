0deecff8ad094cfc628e1319eb7764a329681df9
Emmanuele <emmanuele.piola@gmail.com>
2025-05-29 23:34:55 +0200

    reset

--- app/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-    return (
-      <div className="h-screen w-screen flex items-center justify-center">
-        <span className="text-xl animate-pulse">Caricamento...</span>
-      </div>
-    );
-  }
-  
--- app/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-    return (
-      <div className="h-screen w-screen flex items-center justify-center">
-        <span className="text-xl animate-pulse">404 - Page Not Found</span>
-      </div>
-    );
-  }
-  
